17630114717dea62d3811f1069074441773d54b4
zhonghanlu <1420865757@qq.com>
2023-05-06 16:56:19 +0800

    Improvement

--- gateway-8000/src/main/java/org/zhl/Application.java
+++ gateway-8000/src/main/java/org/zhl/Application.java
@@ -3,11 +3,13 @@ package org.zhl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class
+})
 @EnableDiscoveryClient
-@MapperScan(basePackages = "org.zhl.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- service-user-7001/src/main/java/org/zhl/Application.java
+++ service-user-7001/src/main/java/org/zhl/Application.java
@@ -3,9 +3,11 @@ package org.zhl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@MapperScan(basePackages = "org.mapper")
+@EnableDiscoveryClient
+@MapperScan(basePackages = "org.zhl.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
